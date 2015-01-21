665fedf031b5a0138db9fa7cd28f5f5e6555d4de
Jacob Struzik <jake@robinpowered.com>
2015-01-20 19:20:54 -0500

    trying to get nginx to work

--- provisioning/puppet/manifests/default.pp
+++ provisioning/puppet/manifests/default.pp
@@ -32,7 +32,16 @@ service { $php_application_service:
 # Nginx
 $nginx_app_directory = '/vagrant/app'
 
-class { 'nginx': }
+class { 'nginx':
+    log_format => 
+    [
+        main => '$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$http_x_forwarded_for"'
+    ],
+    server_tokens => 'off',
+    client_max_body_size => '5M',
+    sendfile => 'on',
+    keepalive_timeout => 65
+}
 
 # Ensure that a folder for the app exists
 file { [$nginx_app_directory]:
@@ -42,14 +51,29 @@ file { [$nginx_app_directory]:
 # Nginx virtual hosts
 nginx::resource::vhost { 'app.dev':
     index_files => ['index', 'index.html', 'index.htm', 'index.php'],
+    server_name => 'localhost',
     www_root => $nginx_app_directory,
 }
 
-nginx::resource::location { 'app.dev':
+nginx::resource::location { 'app.dev.php':
+    ensure => present,
+    vhost => 'app.dev',
+    location => '/',
+    rewrite => '/(v[0-9\.]+)/?(.*)/?$ /index.php?$args last',
+    try_files => [
+        /index.php?$args /index /index.html /index.htm /public/index /public/index.html /public/index.htm /public/index.php?$args
+    ]
+}
+
+nginx::resource::location { 'app.dev.php':
+    ensure => present,
     vhost => 'app.dev',
     location => '~ \.php$',
     location_cfg_append => {
+        fastcgi_index => 'index.php',
+        fastcgi_param => '$document_root$fastcgi_script_name',
         fastcgi_intercept_errors => 'on',
         fastcgi_split_path_info => '^(.+\.php)(/.+)$',
+        fastcgi_pass => 127.0.0.1:9000
       }    
 }
